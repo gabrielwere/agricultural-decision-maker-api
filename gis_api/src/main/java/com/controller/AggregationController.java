58c4320f12012435c0fd6a94f3e757707793cdc1
Gabriel <gabriel@healthcloud.co.ke>
2022-03-17 10:51:59 +0300

    feat : add distance calculation functionality

--- gis_api/src/main/java/com/controller/AggregationController.java
+++ gis_api/src/main/java/com/controller/AggregationController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dao.AggregationDAO;
+import com.dao.AirfieldsDAO;
+import com.dao.RoadsDAO;
+import com.dao.UrbanAreasDAO;
 import com.model.Coordinates;
 
 @WebServlet("/aggregation")
@@ -16,20 +19,77 @@ import com.model.Coordinates;
 public class AggregationController extends HttpServlet{
 
     protected void service(HttpServletRequest request,HttpServletResponse response) throws IOException{
-       AggregationDAO aggregationDao = new AggregationDAO();
-       Utility utility = new Utility();
-       
-       ArrayList<Coordinates> aggregationValues = aggregationDao.getAggregation(
-           1000,
-           1200,
-           "well drained",
-           "well",
-           "deep",
-           16,
-           25
-       );
+        AggregationDAO aggregationDao = new AggregationDAO();
+        RoadsDAO roadsDAO = new RoadsDAO();
+        AirfieldsDAO airfieldsDAO = new AirfieldsDAO();
+        UrbanAreasDAO urbanAreasDAO = new UrbanAreasDAO();
         
+        Utility utility = new Utility();
+        
+        ArrayList<Coordinates> aggregationValues = aggregationDao.getAggregation(
+            1000,
+            1200,
+            "well drained",
+            "well",
+            "deep",
+            16,
+            25
+        );
+        
+        ArrayList<Coordinates> roadValues = roadsDAO.getRoads();
+        ArrayList<Coordinates> airfieldValues = airfieldsDAO.getAirfields();
+        ArrayList<Coordinates> urbanAreasValues = urbanAreasDAO.getUrbanAreas();
+        
+        for(int i=0;i<aggregationValues.size();i++){
+            
+            //Shortest road distance calculation
+            for(int j=0;j<roadValues.size();j++){
+                
+                double distance = aggregationValues.get(i).Haversine(roadValues.get(j).getLatitude(), roadValues.get(j).getLongitude());
 
+                //special computation for first iteration
+                if(j==0){
+                    aggregationValues.get(i).setShortestRoadDistance(distance);
+                }else{
+                    if(distance <= aggregationValues.get(i).getShortestRoadDistance()){
+                        aggregationValues.get(i).setShortestRoadDistance(distance);
+                    }
+                }
+            }
+            
+            //Shortest distance to urban areas
+            for(int k=0;k<urbanAreasValues.size();k++){
+                
+                double distance = aggregationValues.get(i).Haversine(urbanAreasValues.get(k).getLatitude(), urbanAreasValues.get(k).getLongitude());
+
+                //special computation for first iteration
+                if(k==0){
+                    aggregationValues.get(i).setShortestUrbanAreasDistance(distance);
+                }else{
+                    if(distance <= aggregationValues.get(i).getShortestUrbanAreasDistance()){
+                        aggregationValues.get(i).setShortestUrbanAreasDistance(distance);
+                    }
+                }
+            }
+            
+            //Shortest distance to airfields
+            for(int x=0;x<airfieldValues.size();x++){
+                
+                double distance = aggregationValues.get(i).Haversine(airfieldValues.get(x).getLatitude(), airfieldValues.get(x).getLongitude());
+
+                //special computation for first iteration
+                if(x==0){
+                    aggregationValues.get(i).setShortestAirfieldDistance(distance);
+                }else{
+                    if(distance <= aggregationValues.get(i).getShortestAirfieldDistance()){
+                        aggregationValues.get(i).setShortestAirfieldDistance(distance);
+                    }
+                }
+            }
+
+        }
+        
+        
         utility.sendCoordinatesAsJson(response,aggregationValues);
     }
     
--- gis_api/src/main/java/com/model/Coordinates.java
+++ gis_api/src/main/java/com/model/Coordinates.java
@@ -4,6 +4,9 @@ public class Coordinates {
 
     private double latitude;
     private double longitude;
+    private double shortestRoadDistance;
+    private double shortestAirfieldDistance;
+    private double shortestUrbanAreasDistance;
 
     public Coordinates(double latitude,double longitude){
         this.latitude = latitude;
@@ -11,11 +14,60 @@ public class Coordinates {
     }
 
     public double getLatitude(){
-        return latitude;
+        return this.latitude;
     }
 
     public double getLongitude(){
-        return longitude;
+        return this.longitude;
+    }
+
+    public double getShortestRoadDistance(){
+        return this.shortestRoadDistance;
+    }
+
+    public double getShortestAirfieldDistance(){
+        return this.shortestAirfieldDistance;
+    }
+
+    public double getShortestUrbanAreasDistance(){
+        return this.shortestUrbanAreasDistance;
+    }
+
+    public void setShortestRoadDistance(double shortestRoadDistance){
+        this.shortestRoadDistance = shortestRoadDistance;
+    }
+
+    public void setShortestAirfieldDistance(double shortestAirfieldDistance){
+        this.shortestAirfieldDistance = shortestAirfieldDistance;
+    }
+
+    public void setShortestUrbanAreasDistance(double shortestUrbanAreasDistance){
+        this.shortestUrbanAreasDistance = shortestUrbanAreasDistance;
+    }
+
+    //formula to calculate distance between two points
+    public double Haversine(double lat2,double long2){
+        double distance;
+        double radius = 6371e3;
+        
+
+        double changeInLatitude = Math.toRadians(lat2-this.latitude);
+        double changeInLongitude = Math.toRadians(long2-this.longitude);
+
+        double lat1 = Math.toRadians(this.latitude);
+        lat2 = Math.toRadians(lat2);
+
+        double a = 
+        (Math.sin(changeInLatitude/2) * Math.sin(changeInLatitude/2)) +
+        Math.cos(lat1) * Math.cos(lat2) *
+        (Math.sin(changeInLongitude/2) * Math.sin(changeInLongitude/2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        //distance in kilometers
+        distance = (radius * c)/1000;
+
+        return distance;
     }
     
 }
